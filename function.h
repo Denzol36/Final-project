1d06d89d621d4f608a59443423a0883c794ee345
Даниил <daniil@MacBook-Air-Daniil.local>
2025-03-22 06:56:07 +0200

    Full work code

--- function.h
+++ function.h
@@ -1,7 +1,17 @@
 #pragma once
 
 #include <iostream>
-#include <dirent.h>
 #include <cstdio>
 #include <cstring>
 using namespace std;
+
+void loadFromFile(const char *filename);
+void saveToFile(const char *filename);
+void addStudent(const char *filename);
+void removeStudent(const char *filename);
+void updateStudent(const char *filename);
+void showAllStudent();
+void searchStudent();
+void sortStudent();
+void showExcellentStudents();
+
--- student.cpp
+++ student.cpp
@@ -1,7 +1,33 @@
 #include <iostream>
+#include <cstring>
+#include "function.h"
 
 using namespace std;
 
-int main(){
+int main() {
+    char filename[100];
+    cout << "Enter the filename: ";
+    fgets(filename, sizeof(filename), stdin);
+    filename[strcspn(filename, "\n")] = 0;
     
+    loadFromFile(filename);
+    int choice;
+    void (*functions[])(const char*) = {addStudent, removeStudent, updateStudent};
+    void (*noArgFunctions[])() = {showAllStudent, searchStudent, sortStudent, showExcellentStudents};
+    
+    do {
+        cout << "\n1. Add Student\n2. Remove Student\n3. Update Student\n4. Show All Students\n";
+        cout << "5. Search Student\n6. Sort Students\n7. Show Excellent Students\n8. Exit\nChoice: ";
+        cin >> choice;
+        
+        if (choice >= 1 && choice <= 3) {
+            functions[choice - 1](filename);
+        } else if (choice >= 4 && choice <= 7) {
+            noArgFunctions[choice - 4]();
+        } else if (choice == 8) {
+            return 0;
+        } else {
+            cout << "Invalid choice!\n";
+        }
+    } while (1);
 }
