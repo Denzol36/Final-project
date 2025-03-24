c2f72831681a61f1f59e37fb564db19991ae7cb1
Даниил <daniil@MacBook-Air-Daniil.local>
2025-03-24 19:45:48 +0200

    Full work code

--- student.cpp
+++ student.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <cstring>
 #include <cstdio>
-#include "function.h"
 
 using namespace std;
 
@@ -9,6 +8,8 @@ struct Student {
     char firstName[50];
     char lastName[50];
     int age;
+    double grades[10];
+    int gradeCount;
     double averageGrade;
 };
 
@@ -17,11 +18,17 @@ int studentCount = 0;
 
 void loadFromFile(const char* filename) {
     FILE* file = fopen(filename, "r");
-    if (!file){
+    if (!file) {
         return;
     }
     studentCount = 0;
-    while (fscanf(file, "%s %s %d %lf", students[studentCount].firstName, students[studentCount].lastName, &students[studentCount].age, &students[studentCount].averageGrade) == 4) {
+    while (fscanf(file, "%s %s %d %d", students[studentCount].firstName, students[studentCount].lastName, &students[studentCount].age, &students[studentCount].gradeCount) == 4) {
+        double sum = 0;
+        for (int i = 0; i < students[studentCount].gradeCount; i++) {
+            fscanf(file, "%lf", &students[studentCount].grades[i]);
+            sum += students[studentCount].grades[i];
+        }
+        students[studentCount].averageGrade = sum / students[studentCount].gradeCount;
         studentCount++;
     }
     fclose(file);
@@ -30,7 +37,11 @@ void loadFromFile(const char* filename) {
 void saveToFile(const char* filename) {
     FILE* file = fopen(filename, "w");
     for (int i = 0; i < studentCount; i++) {
-        fprintf(file, "%s %s %d %.2lf\n", students[i].firstName, students[i].lastName, students[i].age, students[i].averageGrade);
+        fprintf(file, "%s %s %d %d", students[i].firstName, students[i].lastName, students[i].age, students[i].gradeCount);
+        for (int j = 0; j < students[i].gradeCount; j++) {
+            fprintf(file, " %.2lf", students[i].grades[j]);
+        }
+        fprintf(file, "\n");
     }
     fclose(file);
 }
@@ -40,118 +51,69 @@ void addStudent(const char* filename) {
         printf("Student list is full!\n");
         return;
     }
-    printf("Enter first name, last name, age, and average grade: ");
-    scanf("%s %s %d %lf", students[studentCount].firstName, students[studentCount].lastName, &students[studentCount].age, &students[studentCount].averageGrade);
+
+    printf("Enter first name, last name, and age: ");
+    scanf("%s %s %d", students[studentCount].firstName, students[studentCount].lastName, &students[studentCount].age);
+
+    printf("How many grades do you want to enter? (max 10): ");
+    scanf("%d", &students[studentCount].gradeCount);
+
+    if (students[studentCount].gradeCount > 10) {
+        students[studentCount].gradeCount = 10;
+    }
+
+    double sum = 0;
+    printf("Enter %d grades (between 1 and 12):\n", students[studentCount].gradeCount);
+    for (int i = 0; i < students[studentCount].gradeCount; i++) {
+        while (true) {
+            scanf("%lf", &students[studentCount].grades[i]);
+            if (students[studentCount].grades[i] >= 1 && students[studentCount].grades[i] <= 12) {
+                break;
+            }
+            printf("Invalid! Grade must be between 1 and 12. Try again: ");
+        }
+        sum += students[studentCount].grades[i];
+    }
+
+    students[studentCount].averageGrade = sum / students[studentCount].gradeCount;
     studentCount++;
     saveToFile(filename);
 }
 
-void removeStudent(const char* filename) {
-    char lastName[50];
-    printf("Enter last name of student to remove: ");
-    scanf("%s", lastName);
-    for (int i = 0; i < studentCount; i++) {
-        if (strcmp(students[i].lastName, lastName) == 0) {
-            for (int j = i; j < studentCount - 1; j++) {
-                students[j] = students[j + 1];
-            }
-            studentCount--;
-            saveToFile(filename);
-            return;
-        }
-    }
-    printf("Student not found.\n");
-}
-
-void updateStudent(const char* filename) {
-    char lastName[50];
-    printf("Enter last name of student to update: ");
-    scanf("%s", lastName);
-    for (int i = 0; i < studentCount; i++) {
-        if (strcmp(students[i].lastName, lastName) == 0) {
-            printf("Enter new first name, last name, age, and average grade: ");
-            scanf("%s %s %d %lf", students[i].firstName, students[i].lastName, &students[i].age, &students[i].averageGrade);
-            saveToFile(filename);
-            return;
-        }
-    }
-    printf("Student not found.\n");
-}
-
-void showAllStudent() {
+void showAllStudents() {
     if (studentCount == 0) {
         printf("No students to display.\n");
         return;
     }
     for (int i = 0; i < studentCount; i++) {
-        printf("First Name: %s, Last Name: %s, Age: %d, Average Grade: %.2lf\n",students[i].firstName, students[i].lastName, students[i].age, students[i].averageGrade);
-    }
-}
-
-void searchStudent() {
-    char lastName[50];
-    printf("Enter last name of student to search for: ");
-    scanf("%s", lastName);
-    for (int i = 0; i < studentCount; i++) {
-        if (strcmp(students[i].lastName, lastName) == 0) {
-            printf("Student found: First Name: %s, Last Name: %s, Age: %d, Average Grade: %.2lf\n",students[i].firstName, students[i].lastName, students[i].age, students[i].averageGrade);
-            return;
+        printf("First Name: %s, Last Name: %s, Age: %d, Grades: ", students[i].firstName, students[i].lastName, students[i].age);
+        for (int j = 0; j < students[i].gradeCount; j++) {
+            printf("%.2lf ", students[i].grades[j]);
         }
-    }
-    printf("Student not found.\n");
-}
-
-void sortStudent() {
-    for (int i = 0; i < studentCount - 1; i++) {
-        for (int j = i + 1; j < studentCount; j++) {
-            if (students[i].averageGrade < students[j].averageGrade) {
-                Student temp = students[i];
-                students[i] = students[j];
-                students[j] = temp;
-            }
-        }
-    }
-    printf("Students sorted by average grade.\n");
-}
-
-void showExcellentStudents() {
-    bool found = false;
-    for (int i = 0; i < studentCount; i++) {
-        if (students[i].averageGrade >= 9.0) {
-            printf("Excellent Student - First Name: %s, Last Name: %s, Age: %d, Average Grade: %.2lf\n",students[i].firstName, students[i].lastName, students[i].age, students[i].averageGrade);
-            found = true;
-        }
-    }
-    if (!found) {
-        printf("No excellent students found.\n");
+        printf("| Average Grade: %.2lf\n", students[i].averageGrade);
     }
 }
 
 int main() {
     char filename[100];
     cout << "Enter the filename: ";
-    fgets(filename, sizeof(filename), stdin);
-    filename[strcspn(filename, "\n")] = 0;
-    
+    cin >> filename;
+
     loadFromFile(filename);
+
     int choice;
-    void (*functions[])(const char*) = {addStudent, removeStudent, updateStudent};
-    void (*noArgFunctions[])() = {showAllStudent, searchStudent, sortStudent, showExcellentStudents};
-    
-    while(1){
-        cout << "\n1. Add Student\n2. Remove Student\n3. Update Student\n4. Show All Students\n";
-        cout << "5. Search Student\n6. Sort Students\n7. Show Excellent Students\n8. Exit\nChoice: ";
+    while (true) {
+        cout << "\n1. Add Student\n2. Show All Students\n3. Exit\nChoice: ";
         cin >> choice;
-        
-        if (choice >= 1 && choice <= 3) {
-            functions[choice - 1](filename);
-        } else if (choice >= 4 && choice <= 7) {
-            noArgFunctions[choice - 4]();
-        } else if (choice == 8) {
+
+        if (choice == 1) {
+            addStudent(filename);
+        } else if (choice == 2) {
+            showAllStudents();
+        } else if (choice == 3) {
             break;
-        } 
-        else {
+        } else {
             cout << "Invalid choice!\n";
         }
-    };
+    }
 }
